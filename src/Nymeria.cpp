fa04e19b8544bdd87201ee6e0eabccf7309c1aa6
DaBecker <daniela.becker@tu-harburg.de>
2014-11-13 18:53:54 +0100

    Added comments

--- src/Nymeria.cpp
+++ src/Nymeria.cpp
@@ -1,9 +1,22 @@
 #include "Nymeria.h"
 
+/* PRIVATE methods */
+
+/**
+ * Helper function in order to forward user's command
+ * to drone.
+ * Sets drone's navigation state in transmitted message
+ *
+ * @param data - navigation state.
+ */
 void stateDroneCallback (const ardrone_autonomy::Navdata& data){
 	navData.state = data.state;
 }
 
+/**
+ * Procedure that processes incoming command.
+ * @param cmd - incoming command.
+ */
 void Nymeria::nymeriaRoutine(int cmd){
 	if(isSafeAction(cmd))
 		triggerAction(cmd);
@@ -11,8 +24,11 @@ void Nymeria::nymeriaRoutine(int cmd){
 		validateStates(cmd);
 }
 
+/**
+ * Forward command to drone.
+ * @param cmd - incoming command.
+ */
 void Nymeria::triggerAction(int cmd){
-
 	switch(cmd){
 		case M_FORWARD:
 			ROS_INFO("M_FORWARD");
@@ -111,8 +127,17 @@ void Nymeria::modifyStateDrone(int cmd){
 
 }
 /**
+* Check whether current state of drone and current state of
+* obstacle are compatible with user's command.
 *
+* Recursive function, that terminates, when either there is
+* a conflict between states and input command (trigger
+* reactionRoutine) or
+* states and command are compatible and the command has been
+* sent the first time.
 *
+* @param cmd - incoming command: Either M_FORWARD (termination)
+* or CHECK (recursive function call to validateStates(CHECK).
 */
 void Nymeria::validateStates(int cmd){
 	// are the two states in conflict
@@ -136,6 +161,15 @@ void Nymeria::validateStates(int cmd){
 	validateStates(CHECK);
 	return;
 }
+
+/**
+ * Does the user's command belong to the list of safeActions,
+ * i.e. can the command be safely forwarded to the drone?
+ *
+ * @param cmd - incoming command.
+ * @return true: yes, command can be forwarded.
+ * 		   false: no, check for obstacles is necessary.
+ */
 bool Nymeria::isSafeAction(int cmd){
 	for (int i = 0; i < sizeof(safeActions)/sizeof(*safeActions); i++)
 	{
@@ -147,59 +181,149 @@ bool Nymeria::isSafeAction(int cmd){
 	return false;
 }
 
-void Nymeria::reactionRoutine(){}
+/** TODO
+ * Routine in order to make drone stop in front of obstacle and
+ * then bypass it, i.e. navigate around it.
+ */
+void Nymeria::reactionRoutine(){
 
+}
+
+/* PUBLIC methods */
+
+/**
+ * Command in order to move drone forward.
+ */
 void Nymeria::moveForward(){
 	nymeriaRoutine(M_FORWARD);
 }
+
+/**
+ * Command in order to move drone backward.
+ */
 void Nymeria::moveBackward(){
 	nymeriaRoutine(M_BACKWARD);
 }
+
+/**
+ * Command in order to make drone rotate to the left.
+ */
 void Nymeria::moveLeft(){
 	nymeriaRoutine(M_LEFT);
 }
+
+/**
+ * Command in order to make drone rotate to the right.
+ */
 void Nymeria::moveRight(){
 	nymeriaRoutine(M_RIGHT);
 }
+
+/**
+ * Command in order to move drone upward,
+ * i.e. increase altitude.
+ */
 void Nymeria::moveUp(){
 	nymeriaRoutine(M_UP);
 }
+
+/**
+ * Command in order to move drone downward,
+ * i.e. decrease altitude.
+ */
 void Nymeria::moveDown(){
 	nymeriaRoutine(M_DOWN);
 }
+
+/**
+ * Command in order to move drone to the left.
+ */
 void Nymeria::turnLeft(){
 	nymeriaRoutine(T_LEFT);
 }
+
+/**
+ * Command in order to move drone to the right.
+ */
 void Nymeria::turnRight(){
 	nymeriaRoutine(T_RIGHT);
 }
+
+/**
+ * Command in order to stop the drone's movement,
+ * i.e. stay at current position.
+ */
 void Nymeria::stop(){
 	nymeriaRoutine(STOP);
 }
+
+/**
+ * Command in order to make the drone take off.
+ */
 void Nymeria::takeOff(){
 	nymeriaRoutine(TAKEOFF);
 }
+
+/**
+ * Command in order to make the drone land,
+ * i.e. underneath current position.
+ */
 void Nymeria::land(){
 	nymeriaRoutine(LAND);
 }
+
+/**
+ * Command in order to make drone stop and
+ * immediately land.
+ */
 void Nymeria::emergencyStop(){
 	nymeriaRoutine(E_STOP);
 }
+
+/**
+ * Command in order to increase the maximum
+ * speed by 10%.
+ */
 void Nymeria::increaseMaxSpeed(){
 	nymeriaRoutine(I_M_SPEED);
 }
+
+/**
+ * Command in order to decrease the maximum
+ * speed by 10%.
+ */
 void Nymeria::decreaseMaxSpeed(){
 	nymeriaRoutine(D_M_SPEED);
 }
+
+/**
+ * Command in order to increase the linear
+ * speed by 10%.
+ */
 void Nymeria::increaseLinearSpeed(){
 	nymeriaRoutine(I_L_SPEED);
 }
+
+/**
+ * Command in order to decrease the linear
+ * speed by 10%.
+ */
 void Nymeria::decreaseLinearSpeed(){
 	nymeriaRoutine(D_L_SPEED);
 }
+
+/**
+ * Command in order to increase the angular
+ * speed by 10%.
+ */
 void Nymeria::increaseAngularSpeed(){
 	nymeriaRoutine(I_A_SPEED);
 }
+
+/**
+ * Command in order to decrease the angular
+ * speed by 10%.
+ */
 void Nymeria::decreaseAngularSpeed(){
 	nymeriaRoutine(D_A_SPEED);
 }
