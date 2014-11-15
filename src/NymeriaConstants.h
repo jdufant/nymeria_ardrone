9028063652a41ea008978b39ffad74eb87fc3e4d
jdufant <j.dufant@gmail.com>
2014-11-15 18:26:13 +0100

    Create NymeriaConstants.h
    constants class

---
+++ src/NymeriaConstants.h
@@ -0,0 +1,25 @@
+class NymeriaConstants{
+  public:
+    NymeriaConstants();
+    static int CHECK;
+    static int M_FORWARD;
+    static int M_BACKWARD;
+    static int M_LEFT;
+    static int M_RIGHT;
+    static int M_UP;
+    static int M_DOWN;
+    static int T_LEFT;
+    static int T_RIGHT;
+    static int STOP;
+    static int TAKEOFF;
+    static int LAND;
+    static int E_STOP;
+    static int I_M_SPEED;
+    static int D_M_SPEED;
+    static int I_L_SPEED;
+    static int D_L_SPEED;
+    static int I_A_SPEED;
+    static int D_A_SPEED;
+    static int NO_OBSTACLE;
+    static int O_FRONT;
+};
