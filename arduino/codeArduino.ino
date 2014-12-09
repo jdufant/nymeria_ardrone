eeac04b68a36c3b20e57a33913461ab2a06e779b
gburlats <grm@grm-Dell-System-XPS-L702X.(none)>
2014-12-09 18:00:22 +0100

    minor change

--- arduino/codeArduino.ino
+++ arduino/codeArduino.ino
@@ -1,8 +1,4 @@
 const int pingPin = 11;
-long duration;
-int cm;
-int tmp_cm;
-uint8_t first, second, third, last;
 
 void setup() {
   // initialize serial communication:
@@ -13,6 +9,9 @@ void loop()
 {
   // establish variables for duration of the ping, 
   // and the distance result in inches and centimeters:
+  long duration;
+  int cm; int bytesSent;
+  uint8_t first, second, third, last;
 
   // The PING))) is triggered intby a HIGH pulse of 2 or more microseconds.
   // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
@@ -31,20 +30,10 @@ void loop()
 
   // convert the time into a distance
   cm = microsecondsToCentimeters(duration);
-  Serial.flush();
-  if(cm != tmp_cm){
-    String dist = String(cm);
-    
-   
-    if(cm < 10)
-      Serial.print('0');
-    if(cm < 100)
-      Serial.print('0');
-  
-    Serial.print(dist); // always print 3 characters
-    Serial.print('x'); // print 'x'
-  }
-  tmp_cm = cm;
+  String dist = String(cm);
+
+  bytesSent = Serial.print(dist);
+  Serial.print('x');
   delay(10);
 }
 
