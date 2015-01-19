c0151a4ea84ad4ec902f3098c47dc7b292721de9
DaBecker <daniela.becker@tu-harburg.de>
2015-01-19 17:07:27 +0100

    Added doc

--- src/exception/NymeriaExceptions.cpp
+++ src/exception/NymeriaExceptions.cpp
@@ -4,14 +4,27 @@
  * Definition of the class NymeriaExceptions, that defines the base class
  * for all exceptions particular to Nymeria.
  */
+ 
+ /**
+* \file NymeriaExceptions.cpp
+* \class NymeriaExceptions NymeriaExceptions.h
+* \author Team-Nymeria
+* \version 0.2
+* \date 18th of January 2015
+*/
 
+/**
+ * Constructor in order to create an object of type NymeriaException.
+ **/
 NymeriaExceptions::NymeriaExceptions(string msg){
 	this->errMsg = msg;
 }
 
 NymeriaExceptions::~NymeriaExceptions(void) throw(){}
 
-/* Overriding what() function from standard Exception. */
+/**
+ * Overriding what() function from standard Exception.
+ **/
 const char * NymeriaExceptions::what() const throw(){
 	/* display error message */
 	return this->errMsg.c_str();
