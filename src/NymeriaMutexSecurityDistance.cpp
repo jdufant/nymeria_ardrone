fb3bc243b9ae37f2a2fd5e251d9872250793127b
DaBecker <daniela.becker@tu-harburg.de>
2015-01-20 10:44:38 +0100

    added doc

--- src/NymeriaMutexSecurityDistance.cpp
+++ src/NymeriaMutexSecurityDistance.cpp
@@ -1,19 +1,46 @@
 #include <nymeria_ardrone/NymeriaMutexSecurityDistance.h>
-#include <iostream> // to get NULL
+#include <iostream>
 
+/**
+* Defintion of the class NymeriaMutexSecurityDistance, which manages access to the ROS Parameter nymeriaSecurityDistance.
+**/
+
+/**
+* Default constructor in order to create an object of type NymeriaMutexSecurityDistance.
+**/
 NymeriaMutexSecurityDistance::NymeriaMutexSecurityDistance(){};
 
+/**
+* Destructor resetting class attributes.
+*/
 NymeriaMutexSecurityDistance::~NymeriaMutexSecurityDistance(){
   instanceFlag = false;
   locked = false;
 };
 
-bool NymeriaMutexSecurityDistance::instanceFlag = false;
 
+/**
+* Flag in order to make sure there is only one instance of the class.
+* true - has been already instantiated once.
+* false - hasn't been instantiated yet.
+*/
+bool NymeriaMutexSecurityDistance::instanceFlag = false;
+/**
+* Attribute that marks whether or not the resource has been acquired yet.
+* true - has been already acquired.
+* false - hasn't been acquired yet.
+*/
 bool NymeriaMutexSecurityDistance::locked = false;
 
+/**
+* First declaration of instance of type NymeriaMutexSecurityDistance.
+*/
 NymeriaMutexSecurityDistance * NymeriaMutexSecurityDistance::mutexSecDist = NULL;
 
+/**
+* Function in order to get an instance of NymeriaMutexSecurityDistance.
+* @return useful, i.e. not NULL object of type NymeriaMutexSecurityDistance.
+*/
 NymeriaMutexSecurityDistance * NymeriaMutexSecurityDistance::getInstance(){
   if(! instanceFlag){
     mutexSecDist = new NymeriaMutexSecurityDistance();
@@ -25,11 +52,19 @@ NymeriaMutexSecurityDistance * NymeriaMutexSecurityDistance::getInstance(){
   }
 }
 
+/**
+* Method in order to lock or acquire resource.
+* Resource can not be acquired by any other object while being locked.
+*/
 void NymeriaMutexSecurityDistance::lock(){
   while(locked){}
   locked = true;
 }
 
+/**
+* Method in order to unlock or release resource.
+* Resource can be acquired by an other object after being released.
+*/
 void NymeriaMutexSecurityDistance::unlock(){
   locked = false;
 }
