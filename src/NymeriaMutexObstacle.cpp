ca9bf3e4a8ecb8bcf9875c3f2210aa41ae8c0b9e
DaBecker <daniela.becker@tu-harburg.de>
2015-01-20 09:37:58 +0100

    added doc

--- src/NymeriaMutexObstacle.cpp
+++ src/NymeriaMutexObstacle.cpp
@@ -1,16 +1,44 @@
 #include <nymeria_ardrone/NymeriaMutexObstacle.h>
-#include <iostream> // to get NULL
+#include <iostream>
 
+/**
+* Defintion of the class NymeriaMutexObstacle, which manages access to the ROS Parameter nymeriaStateObstacle.
+**/
+
+/**
+* Default constructor in order to create an object of type NymeriaStateObstacle.
+**/
 NymeriaMutexObstacle::NymeriaMutexObstacle(){};
+
+/**
+* Destructor resetting class attributes.
+*/
 NymeriaMutexObstacle::~NymeriaMutexObstacle(){
 	instanceFlag = false;
 	locked = false;
 };
 
+/**
+* Flag in order to make sure there is only one instance of the class.
+* true - has been already instantiated once.
+* false - hasn't been instantiated yet.
+*/
 bool NymeriaMutexObstacle::instanceFlag = false;
+/**
+* Attribute that marks whether or not the resource has been acquired yet.
+* true - has been already acquired.
+* false - hasn't been acquired yet.
+*/
 bool NymeriaMutexObstacle::locked = false;
+/**
+* First declaration of instance of type NymeriaMutex.
+*/
 NymeriaMutexObstacle * NymeriaMutexObstacle::mutexObstacle = NULL;
 
+/**
+* Function in order to get an instance of NymeriaMutexObstacle.
+* @return useful, i.e. not NULL object of type NymeriaMutex.
+*/
 NymeriaMutexObstacle * NymeriaMutexObstacle::getInstance(){
 	if(! instanceFlag){
 		mutexObstacle = new NymeriaMutexObstacle();
@@ -22,11 +50,19 @@ NymeriaMutexObstacle * NymeriaMutexObstacle::getInstance(){
 	}
 }
 
+/**
+* Method in order to lock or acquire resource.
+* Resource can not be acquired by any other object while being locked.
+*/
 void NymeriaMutexObstacle::lock(){
 	while(locked){}
 	locked = true;
 }
 
+/**
+* Method in order to unlock or release resource.
+* Resource can be acquired by an other object after being released.
+*/
 void NymeriaMutexObstacle::unlock(){
 	locked = false;
 }
